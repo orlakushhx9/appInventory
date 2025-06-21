2ed468c61ff4513785b87a0c146874c38594a675
orlakushhx9 <orlandosifuentes23@gmail.com>
2025-06-20 23:12:11 -0600

    ultimos cambios finales para despligue de pruebas

---
+++ app/src/main/java/com/example/InventarioApp/ui/auth/ForgotPasswordConfirmationActivity.kt
@@ -0,0 +1,13 @@
+package com.example.InventarioApp.ui.auth
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.InventarioApp.R
+
+class ForgotPasswordConfirmationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_forgot_password_confirmation)
+
+    }
+} 
